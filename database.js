4fac9c8d0f079cf0a22a2b6fd0d7d83d9515e186
Daniel Phillips <dphildev@gmail.com>
2016-05-25 10:22:25 +0100

    Keeping track of games that are no longer in the index.

--- database.js
+++ database.js
@@ -12,22 +12,36 @@ function populateGameTable() {
             id INTEGER PRIMARY KEY AUTOINCREMENT,\
             name TEXT,\
             url TEXT,\
+            available INTEGER,\
             UNIQUE(url)\
         )");
         
+        db.run("UPDATE games SET available = 0");
+        
         var gameInsert = db.prepare("\
-            INSERT INTO games (name, url) SELECT ?, ?\
+            INSERT INTO games (name, url, available) SELECT ?, ?, 1\
             WHERE NOT EXISTS(SELECT 1 FROM games WHERE url = ?2)\
         ");
+        var gameUpdateAvailable = db.prepare("UPDATE games SET available = 1 WHERE url = ?");
+        
         gameData.forEach(function (game) {
             gameInsert.run(game.name, game.url);
+            gameUpdateAvailable.run(game.url);
         });
+        gameUpdateAvailable.finalize();
         gameInsert.finalize();
-        /*
-        db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-            console.log(row.id + ": " + row.info);
-        });*/
     });
 }
+
+function createUrlList() {
+    db.serialize(function () {
+        db.each("SELECT id, url FROM games WHERE available = 1", function (err, row) {
+            console.log(row.id + ": " + row.url);
+        });
+    });
+}
+
+populateGameTable();
+createUrlList();
 
 db.close();
